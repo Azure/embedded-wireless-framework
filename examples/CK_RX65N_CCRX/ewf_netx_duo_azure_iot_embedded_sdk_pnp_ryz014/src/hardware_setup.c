5fcab7ba0779c341755a56db996d45dc0cef639f
YusukeTakahashiGithub <yusuke.takahashi.xg@renesas.com>
2023-02-10 14:09:12 +0900

    Add ewf_netx_duo_azure_iot_embedded_sdk_pnp_ryz014

---
+++ examples/CK_RX65N_CCRX/ewf_netx_duo_azure_iot_embedded_sdk_pnp_ryz014/src/hardware_setup.c
@@ -0,0 +1,28 @@
+
+#include "r_smc_entry.h"
+
+#include <platform.h>
+
+#include <r_cmt_rx_if.h>
+
+#include <tx_api.h>
+
+#include "hardware_setup.h"
+
+void _tx_timer_interrupt(void);
+
+/* CMT Timer callback used as the system tick. */
+void timer_callback(void * pdata)
+{
+    _tx_timer_interrupt();
+}
+
+
+void platform_setup(void)
+{
+    uint32_t chan;
+    
+    /* Create periodic timer for the system tick. */
+    R_CMT_CreatePeriodic(TX_TIMER_TICKS_PER_SECOND, timer_callback, &chan);
+
+}
